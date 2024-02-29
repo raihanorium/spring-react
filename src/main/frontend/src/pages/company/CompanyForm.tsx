cccc2c7d98b5e42360fd06d929725467fdb291c1
Raihan <raihan.dk@gmail.com>
2024-02-29 08:18:32 +0600

    Company edit

--- src/main/frontend/src/pages/company/CompanyForm.tsx
+++ src/main/frontend/src/pages/company/CompanyForm.tsx
@@ -4,6 +4,7 @@ import {CButton, CCol, CForm, CFormInput, CRow, CSpinner} from "@coreui/react";
 import {Company} from "../../model/Company";
 import {useNavigate, useParams} from "react-router-dom";
 import {useCompanyService} from "../../service/useService";
+import {SpinnerContainer} from "../../utils/SpinnerContainer";
 
 export default function CompanyForm() {
     const companyService = useCompanyService();
@@ -11,6 +12,8 @@ export default function CompanyForm() {
     const [submitting, setSubmitting] = useState(false)
     const navigate = useNavigate();
     const {companyId} = useParams();
+    const [company, setCompany] = useState<Company | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -35,50 +38,59 @@ export default function CompanyForm() {
     }
 
     useEffect(() => {
-        if (companyId && companyService !== null) {
-            companyService.getCompany(Number(companyId)).then(company => {
-                if (company) {
-                    console.log(company);
+        if (companyId && companyService) {
+            setLoading(true);
+            companyService.getCompany(Number(companyId)).then(c => {
+                if (c) {
+                    setCompany(c);
                 }
-            });
+            }).finally(() => setLoading(false));
+        } else {
+            setCompany(null);
         }
     }, [companyId]);
 
     return (
-        <CForm className="row g-3 needs-validation pt-2"
-               noValidate
-               validated={validated}
-               onSubmit={handleSubmit}>
-            <CRow className="mb-3">
-                <CCol>
-                    {companyId && <p>{companyId}</p>}
-                    <CFormInput name="companyName" type="text" id="companyName" label="Company Name" required
-                                feedbackInvalid="Put a company name"/>
-                </CCol>
-            </CRow>
-            <CRow className="mb-3">
-                <CCol>
-                    <CFormInput name="contactPerson" type="text" id="contactPerson" label="Contact Person Name"/>
-                </CCol>
-            </CRow>
-            <CRow className="mb-3">
-                <CCol>
-                    <CFormInput type="text" name="contactNo" id="contactNo" label="Contact Number"/>
-                </CCol>
-            </CRow>
-            <CRow className="mb-3">
-                <CCol>
-                    <CFormInput type="text" name="officeAddress" id="officeAddress" label="Office Address"/>
-                </CCol>
-            </CRow>
-            <CRow className="mb-3">
-                <CCol>
-                    <CButton color="primary" type="submit" disabled={submitting}>
-                        {submitting && <CSpinner component="span" size="sm" aria-hidden="true" className="me-2"/>}
-                        Save
-                    </CButton>
-                </CCol>
-            </CRow>
-        </CForm>
+        <SpinnerContainer loading={loading}>
+            <CForm className="row g-3 needs-validation pt-2"
+                   noValidate
+                   validated={validated}
+                   onSubmit={handleSubmit}>
+                <CRow className="mb-3">
+                    <CCol>
+                        {companyId && <CFormInput name="id" type="hidden" id="id" value={companyId}/>}
+
+                        <CFormInput name="companyName" type="text" id="companyName" label="Company Name" required
+                                    feedbackInvalid="Put a company name" defaultValue={company?.name ?? undefined}/>
+                    </CCol>
+                </CRow>
+                <CRow className="mb-3">
+                    <CCol>
+                        <CFormInput name="contactPerson" type="text" id="contactPerson" label="Contact Person Name"
+                                    defaultValue={company?.contactPerson ?? undefined}/>
+                    </CCol>
+                </CRow>
+                <CRow className="mb-3">
+                    <CCol>
+                        <CFormInput type="text" name="contactNo" id="contactNo" label="Contact Number"
+                                    defaultValue={company?.contactNo ?? undefined}/>
+                    </CCol>
+                </CRow>
+                <CRow className="mb-3">
+                    <CCol>
+                        <CFormInput type="text" name="officeAddress" id="officeAddress" label="Office Address"
+                                    defaultValue={company?.officeAddress ?? undefined}/>
+                    </CCol>
+                </CRow>
+                <CRow className="mb-3">
+                    <CCol>
+                        <CButton color="primary" type="submit" disabled={submitting}>
+                            {submitting && <CSpinner component="span" size="sm" aria-hidden="true" className="me-2"/>}
+                            Save
+                        </CButton>
+                    </CCol>
+                </CRow>
+            </CForm>
+        </SpinnerContainer>
     );
 }
